ac6329f2139823e4a43b18cf5fc35f38ed449092
Zhuyu Chen Su <99259811+2huyu@users.noreply.github.com>
2022-08-28 19:10:23 -0300

    Add files via upload

---
+++ calculobasico.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class calculobasico {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		double calculo1, calculo2;
+		double operacoes[] = new double[adi, sub, multi, div, res, pow];
+		
+		res = calculo1 % calculo2;
+		pow = Math.pow(calculo1, calculo2);
+		
+		for (int i = 0; i < operacoes.length; i++) {
+			System.out.println("Digite dois números para calcular potência");
+			calculo1 = entrada.nextDouble();
+			calculo2 = entrada.nextDouble();
+			operacoes[i] = calculo1 + calculo2;
+			System.out.println(operacoes[i]);
+		}
+	}
+}
